1d5d170a3067e481d4ae942eb4e72f7f42630198
Barry Gaffey <bgaffey@umassd.edu>
2016-03-10 18:45:44 -0500

    Made a change.

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -6,6 +6,9 @@ WinMain(HINSTANCE hInstance,
 		LPSTR lpCmdLine,
 		int nCmdShow)
 {
+#if _DEBUG
+	OutputDebugString(L"Running in debug mode.\n");
+#endif
 	MessageBox(0, L"Press OK.", L"The New App",
 			   MB_OK | MB_ICONINFORMATION);
 	return(0);
