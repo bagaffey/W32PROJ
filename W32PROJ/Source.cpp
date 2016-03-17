d39db129b6783e7e2e70c350faac6f0052ff5831
Barry Gaffey <bgaffey@umassd.edu>
2016-03-16 20:50:22 -0400

    Added new function G().

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -1,13 +1,24 @@
 #include <Windows.h>
+#include <stdio.h>
 #if _DEBUG
 #include <crtdbg.h>
 #endif
 
+void
+G(void)
+{
+	UINT16 a0 = 9;
+	a0 |= 1;
+	WCHAR TextBuffer[512];
+	_snwprintf_s(TextBuffer, sizeof(TextBuffer), L"%d\n", a0);
+	OutputDebugString(TextBuffer);
+}
+
 int 
 F(void)
 {
-	void* x = malloc(512);
 #if _DEBUG
+	void* x = malloc(512);
 	return (_CrtDumpMemoryLeaks());
 #endif
 }
@@ -19,6 +30,7 @@ WinMain(HINSTANCE hInstance,
 		int nCmdShow)
 {
 	F();
+	G();
 	MessageBox(0, L"Press OK.", L"The New App", 
 		MB_OKCANCEL | MB_ICONINFORMATION);
 	return(0);
