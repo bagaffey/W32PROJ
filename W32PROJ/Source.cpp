24ec4bad61777ba3b2b7735d621972f58a155f9c
Barry Gaffey <bgaffey@umassd.edu>
2016-06-14 11:53:51 -0400

    A test.

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -3,24 +3,22 @@
 #if _DEBUG
 #include <crtdbg.h>
 #endif
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
 
 void
-G(void)
+F(void)
 {
 	UINT16 a0 = 9;
 	a0 |= 1;
 	WCHAR TextBuffer[512];
-	_snwprintf_s(TextBuffer, sizeof(TextBuffer), L"%d\n", a0);
+	_snwprintf_s(TextBuffer, sizeof(TextBuffer), L"The answer is always %d\n", a0);
 	OutputDebugString(TextBuffer);
-}
 
-int 
-F(void)
-{
-#if _DEBUG
-	void* x = malloc(512);
-	return (_CrtDumpMemoryLeaks());
-#endif
+	char str[128] = "";
+	sprintf(str, "%s %s", str, "FUNC");
+	sprintf(str, "%s %s", str, "0");
+	OutputDebugStringA(str);
 }
 
 int CALLBACK
@@ -30,7 +28,6 @@ WinMain(HINSTANCE hInstance,
 		int nCmdShow)
 {
 	F();
-	G();
 	MessageBox(0, L"Press OK.", L"The New App", 
 		MB_OKCANCEL | MB_ICONINFORMATION);
 	return(0);
