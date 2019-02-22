61a8643def204b1407d8b2a41baee2cc118b9e64
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-02-21 22:32:04 -0500

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -95,6 +95,15 @@ void Concat2(char s[256], char t[256])
 	OutputDebugStringA(output);
 }
 
+void
+F7(void)
+{
+	UINT8 a0 = 2;
+	WCHAR TextBuffer[512];
+	_snwprintf_s(TextBuffer, sizeof(TextBuffer), L"\nThe answer is now %d\n", a0 & 2);
+	OutputDebugString(TextBuffer);
+}
+
 int CALLBACK
 WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
