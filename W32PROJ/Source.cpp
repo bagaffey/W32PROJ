48a928ad6bd46c8aeb1167efa67dc6a5f1d7cedc
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-11-22 22:36:17 -0500

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -108,7 +108,7 @@ WinMain(HINSTANCE hInstance,
 	F2();
 	
 	F4();
-	MessageBox(0, L"Maximum memory utilization reached.", L"Battlecruiser Application",
+	MessageBox(0, L"Battlecruiser operational.", L"Battlecruiser Application",
 		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION);
 	return(0);
 }
