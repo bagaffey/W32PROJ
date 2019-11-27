fec5e76a5b7552ceeaccd714918bf2a2047dd561
Barry Gaffey <bagaffey@users.noreply.github.com>
2019-11-26 22:34:30 -0500

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -108,7 +108,7 @@ WinMain(HINSTANCE hInstance,
 	F2();
 	
 	F4();
-	char * ValueString;
+	
 	MessageBox(0, L"Battlecruiser operational.", L"Battlecruiser Application",
 		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION);
 	return(0);
