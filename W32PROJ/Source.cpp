7689e25c62797d49a8f7c83fc8ed6e3efa391329
Barry Gaffey <bagaffey@users.noreply.github.com>
2020-03-27 17:49:10 -0400

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -108,7 +108,7 @@ WinMain(HINSTANCE hInstance,
 	F();
 	F2();
 	F4();
-	// The sickness
+	// Right to the door
 	
 	MessageBox(0, L"Battlecruiser operational.", L"Battlecruiser Application",
 		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION);
