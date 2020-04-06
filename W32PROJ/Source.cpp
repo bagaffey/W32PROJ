57addaa16b6dcb604052ceec90517ff8389fca67
Barry Gaffey <bagaffey@users.noreply.github.com>
2020-04-05 22:33:27 -0400

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -108,7 +108,7 @@ WinMain(HINSTANCE hInstance,
 	F();
 	F2();
 	F4();
-	// time up
+	// overwhelmed.
 	
 	MessageBox(0, L"Battlecruiser operational.", L"Battlecruiser Application",
 		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION);
