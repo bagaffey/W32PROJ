619291512c57d87e700dde1a271b8af5ad37e562
Barry Gaffey <bagaffey@users.noreply.github.com>
2020-04-09 09:01:24 -0400

    Update Source.cpp

--- W32PROJ/Source.cpp
+++ W32PROJ/Source.cpp
@@ -108,7 +108,7 @@ WinMain(HINSTANCE hInstance,
 	F();
 	F2();
 	F4();
-	// Gone with the wind.
+	// Processing plant
 	
 	MessageBox(0, L"Battlecruiser operational.", L"Battlecruiser Application",
 		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION);
